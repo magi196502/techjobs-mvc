cbca49830b59b9bd65d1624c339b8b064306c1aa
Macy Graham-Kimbrough <macyg196502@yahoo.com>
2019-01-23 05:38:42 -0600

    Finish techjobs-mvc

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,6 +4,7 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -23,5 +24,21 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value="results")
+    public String processSearchResults(Model model, @RequestParam String searchType,
+                                       @RequestParam String searchTerm ) {
 
+        ArrayList<HashMap<String, String>> jobs;
+
+        if (searchType.equals("all") || searchTerm.equals("")) {
+            jobs = JobData.findByValue(searchTerm);
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType,searchTerm);
+        }
+
+        model.addAttribute("columns", ListController.columnChoices);
+        model.addAttribute("jobs", jobs);
+
+        return "search";
+    }
 }
